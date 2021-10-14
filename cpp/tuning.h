1f00eaa53b2d0e34f68be74f596c0ac902e12741
Daniel Zoba <daniel.sobe@sorben.com>
2021-10-14 15:58:56 +0200

    forgot header file

---
+++ cpp/tuning.h
@@ -0,0 +1,30 @@
+#ifndef USB_MIC_ARRAY_TUNING_H
+#define USB_MIC_ARRAY_TUNING_H
+
+#include <libusb-1.0/libusb.h>
+
+typedef void(*usb_mic_array__vad_cb_fn)(int active);
+
+int usb_mic_array__find_open_usb_device(
+	libusb_device_handle **devHandle, 
+	libusb_context **context, 
+	uint8_t *interfaceNumber, 
+	uint16_t vendorId, 
+	uint16_t productId);
+
+int usb_mic_array__vad_request(
+	libusb_device_handle *devHandle,
+	usb_mic_array__vad_cb_fn callback
+	);
+
+int usb_mic_array__vad_process(
+	libusb_context *context);
+
+void usb_mic_array__vad_cleanup();
+
+int usb_mic_array__close_usb_device(
+	libusb_device_handle *devHandle,
+	libusb_context *context 
+	);
+
+#endif
